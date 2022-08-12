8c9cfbf741fedd800cab2adb84496b9458364606
Pichau <gustavosandrindattein123@gmail.com>
2022-08-12 14:05:56 -0300

    retornado o filtro

--- brasileirao/jogadores/views.py
+++ brasileirao/jogadores/views.py
@@ -1,5 +1,5 @@
-
-from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets
 
 from brasileirao.jogadores.models import Jogador
 from brasileirao.jogadores.serializers import JogadorSerializer
@@ -8,5 +8,5 @@ from brasileirao.jogadores.serializers import JogadorSerializer
 class JogadorViewSet(viewsets.ModelViewSet):
     queryset = Jogador.objects.all()
     serializer_class = JogadorSerializer
-    filter_backends = [filters.SearchFilter]
+    filter_backends = [DjangoFilterBackend]
     filterset_fields = ['nome', 'time', 'idade', 'posicao']
